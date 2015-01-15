a31ead216b8d363740aa6da67995a287791e3a0b
David Bayer <david.bayer@icainformatics.com>
2015-01-15 16:21:05 -0600

    added helper functions Get-ScriptDirectory and Initialize-MySQLDriver

--- PSMySQL.psm1
+++ PSMySQL.psm1
@@ -10,12 +10,53 @@
 	===========================================================================
 #>
 
-function Write-HelloWorld
-{
-	Write-Host "Hello World"
+<#
+	.SYNOPSIS
+		Load the MySQL Driver into memory.
+	
+	.DESCRIPTION
+		Loads the MySQL driver into memory for the current session.
+		
+		Possible return codes:
+		0	Success
+		1	Driver file not found
+		2	Error loading driver
+#>
+function Initialize-MySQLDriver {
+	[CmdletBinding()]
+	[OutputType([int])]
+	
+	$exitCode = 0
+	
+	$scriptDir = Get-ScriptDirectory
+	$mysqlDriver = "$scriptDir\mysql.data.dll"
+	
+	if (Test-Path -Path $mysqlDriver) {
+		try {
+			Add-Type -Path $mysqlDriver
+		} catch {
+			$exitCode = 2
+		}
+	} else {
+		$exitCode = 1
+	}
+	
+	return $exitCode
 }
-
-Export-ModuleMember Write-HelloWorld
-
-
+	
+	
+<#
+	.SYNOPSIS
+		Returns the directory of the script file.
+	
+	.DESCRIPTION
+		Returns the directory of the script file.
+#>
+function Get-ScriptDirectory {
+	[CmdletBinding()]
+	[OutputType([string])]
+	
+	$scriptdir = $script:MyInvocation.MyCommand.Path
+	return $scriptdir
+}
 
